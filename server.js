8c66bf83e53ece56f2dcbb75d0a222aba575e12d
tathagata218 <tathagata218@yahoo.com>
2017-12-08 12:22:35 -0600

    this is the stuff

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+
+let app = express();
+let PORT = 8080;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use("/public", express.static("public"));
+
+
+app.listen(PORT, function() {
+    console.log("its working!!")
+});
